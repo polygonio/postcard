f85e6232f7f5a1c89b8bd73faac4205ebac0d6a0
David Gregoire <david@Davids-Laptop.local>
2015-02-01 14:38:56 -0800

    challenge 1 and 2

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
         
     
     override func viewDidLoad() {
@@ -33,8 +34,19 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        //This is where I am going to try to change text
+        nameLabel.hidden=false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        
+        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
+        
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
